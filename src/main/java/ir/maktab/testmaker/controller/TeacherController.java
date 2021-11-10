1cd0ba2df4d7ff9ca050a635b4122e4e631fa7c7
SadegH <sadegh.iraji@ymail.com>
2021-11-10 12:04:58 +0330

    add teacher controller methods till add new questions

--- src/main/java/ir/maktab/testmaker/controller/TeacherController.java
+++ src/main/java/ir/maktab/testmaker/controller/TeacherController.java
@@ -1,11 +1,8 @@
 package ir.maktab.testmaker.controller;
 
-import ir.maktab.testmaker.model.Course;
-import ir.maktab.testmaker.model.Teacher;
-import ir.maktab.testmaker.model.Test;
-import ir.maktab.testmaker.service.CourseService;
-import ir.maktab.testmaker.service.TeacherService;
-import ir.maktab.testmaker.service.TestService;
+import ir.maktab.testmaker.model.*;
+import ir.maktab.testmaker.model.enumeration.QType;
+import ir.maktab.testmaker.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -29,11 +26,23 @@ public class TeacherController {
 
     private final TestService testService;
 
+    private final TasqService tasqService;
+
+    private final DescriptiveQuestionService descriptiveQuestionService;
+
+    private final QuestionService questionService;
+
+    private final SelectionService selectionService;
+
+    private final MCQService mcqService;
+
+    // Main Menu of Teacher
     @GetMapping("/teacherMenu")
     public String teacherMenu() {
         return "/teacher/teacherMenu";
     }
 
+    // Show Courses Teaching by the Teacher
     @GetMapping("/teacherCourses")
     public String teacherCourses(ModelMap modelMap) {
         String activeUsername = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -44,6 +53,7 @@ public class TeacherController {
         return "/teacher/teacherCourses";
     }
 
+    // List of Tests made for a course
     @PostMapping("courseTests")
     public String courseTests(@RequestParam String courseId,
                               @RequestParam String teacherId,
@@ -97,5 +107,117 @@ public class TeacherController {
             modelMap.addAttribute("message", "مشکلی پیش آمده است");
         }
         return "/teacher/deleteTest";
+    }
+
+    @PostMapping("deleteQuestion")
+    @Transactional
+    public String deleteQuestion(@RequestParam String questionId,
+                                 ModelMap modelMap) {
+        try {
+            Question question = questionService.findQuestionById(Long.parseLong(questionId));
+            questionService.delete(question);
+        } catch (Exception e) {
+            e.printStackTrace();
+            modelMap.addAttribute("message", "مشکلی پیش آمده است");
+        }
+        return "/teacher/deleteQuestion";
+    }
+
+    @PostMapping("editTest")
+    public String editTest(@RequestParam String testId,
+                           ModelMap modelMap) {
+        Test test = testService.findTestById(Long.parseLong(testId));
+        List<Tasq> tasqList = tasqService.findTasqsByTest(test);
+        modelMap.addAttribute("test", test);
+        if (tasqList.isEmpty()) {
+            modelMap.addAttribute("message", "سوالی برای این آزمون ثبت نشده است");
+        } else {
+            modelMap.addAttribute("tasqList", tasqList);
+        }
+        return "/teacher/editTest";
+    }
+
+    @PostMapping("addNewQuestion")
+    public String addNewQuestion(@RequestParam String testId,
+                                 ModelMap modelMap) {
+        Test test = testService.findTestById(Long.parseLong(testId));
+        modelMap.addAttribute("test", test);
+        return "/teacher/addNewQuestion";
+    }
+
+    @PostMapping("addNewDQ")
+    public String addNewDQ(@RequestParam String testId,
+                           ModelMap modelMap) {
+        Test test = testService.findTestById(Long.parseLong(testId));
+        modelMap.addAttribute("test", test);
+        return "/teacher/addNewDQ";
+    }
+
+    @PostMapping("addNewDQConfirm")
+    @Transactional
+    public String addNewDQConfirm(@RequestParam String title,
+                                  @RequestParam String content,
+                                  @RequestParam String testId,
+                                  @RequestParam String score,
+                                  ModelMap modelMap) {
+        try {
+            Test test = testService.findTestById(Long.parseLong(testId));
+            Course course = test.getCourse();
+            Teacher teacher = course.getTeacher();
+            double qScore = Double.parseDouble(score);
+            // Add new question to database and to course and teacher bank
+            DescriptiveQuestion descriptiveQuestion = new DescriptiveQuestion(title, content, QType.DQ
+                    , course, teacher);
+            descriptiveQuestionService.save(descriptiveQuestion);
+            // Map Question, Test and Score of question in the test
+            Tasq tasq = new Tasq(qScore, descriptiveQuestion, test);
+            tasqService.save(tasq);
+        } catch (Exception e) {
+            e.printStackTrace();
+            modelMap.addAttribute("message", "مشکلی پیش آمده است");
+        }
+        return "/teacher/addNewDQConfirm";
+    }
+
+    @PostMapping("addNewMSQ")
+    public String addNewMSQ(@RequestParam String testId,
+                            ModelMap modelMap) {
+        modelMap.addAttribute("testId", testId);
+        return "/teacher/addNewMSQ";
+    }
+
+    @PostMapping("addNewMSQConfirm")
+    @Transactional
+    public String addNewMSQConfirm(@RequestParam(value = "options[]") String[] options,
+                                   @RequestParam String title,
+                                   @RequestParam String content,
+                                   @RequestParam String testId,
+                                   @RequestParam String score,
+                                   @RequestParam String answer,
+                                   ModelMap modelMap) {
+        Test test = testService.findTestById(Long.parseLong(testId));
+        Course course = test.getCourse();
+        Teacher teacher = course.getTeacher();
+        boolean isAnswer = false;
+        try {
+            // Add new question to database and to course and teacher bank
+            MultiChoiceQuestion multiChoiceQuestion = new MultiChoiceQuestion(title, content,
+                    QType.MCQ, course, teacher);
+            mcqService.save(multiChoiceQuestion);
+            // Add Every Options to database and map to question
+            for (String option : options) {
+                // Check if option is equals answer ignoring white spaces ......................................
+                isAnswer = option.replaceAll("\\s+", "")
+                        .equalsIgnoreCase(answer.replaceAll("\\s+", ""));
+                selectionService.save(new Selection(option, isAnswer, multiChoiceQuestion));
+            }
+            // Map Question, Test and Score of question in the test
+            tasqService.save(new Tasq(Double.parseDouble(score), multiChoiceQuestion, test));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            modelMap.addAttribute("message", "مشکلی پیش آمده است");
+        }
+        return "/teacher/addNewMSQConfirm";
     }
 }
